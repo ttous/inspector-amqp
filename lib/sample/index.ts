d4df29813dbec2d2053c369de14c99a8eeae775a
ttoussaint <teddy.toussaint@nexworld.fr>
2019-03-21 17:08:02 +0100

    Simplify sample

--- lib/sample/index.ts
+++ lib/sample/index.ts
@@ -1,19 +1,16 @@
 import { Event } from "inspector-metrics";
 import { AmqpMetricReporter, AmqpTopologyHelper } from "../metrics";
 
-// instance the Amqp reporter
+// Instantiate the AMQP reporter
 const reporter: AmqpMetricReporter = new AmqpMetricReporter({
   amqpTopologyBuilder: AmqpTopologyHelper.queue("amqp://localhost", "queue"),
 });
 
-// start reporter
-reporter.start().then((r) => {
-  const event = new Event<{}>("test")
-    .setValue({
-      int: 123,
-      string: "toto",
-    });
-
-  // send event
-  r.reportEvent(event);
+// Create the event
+const event = new Event<{}>("test").setValue({
+  int: 123,
+  string: "toto",
 });
+
+// Report the event
+reporter.reportEvent(event);
